076502aa010f3dfa0cbce69046d564bae1f1ca73
Jerome Hardaway <jerome@vetswhocode.io>
2017-11-13 21:42:34 -0600

    initial design

---
+++ src/app/app,component.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  'movable': {
+    'background': 'coral',
+    'height': [{ 'unit': 'px', 'value': 200 }],
+    'width': [{ 'unit': 'px', 'value': 200 }],
+    'textAlign': 'center',
+    'fontSize': [{ 'unit': 'px', 'value': 20 }],
+    'color': 'white',
+    'fontWeight': 'bold',
+    'marginBottom': [{ 'unit': 'px', 'value': 10 }],
+    'position': 'relative'
+  }
+});
---
+++ src/app/app.component.ts
@@ -0,0 +1,31 @@
+import { Component, trigger, state, style, animate, transition } from '@angular/core';
+
+@Component({
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css'],
+	// animations metadata, we define a trigger with two possible states
+	animations: [
+		trigger('movable', [
+			state('fixed', style({})),
+			state(
+				'roaming',
+				style({
+					'background-color': 'green',
+					left: '90%'
+				})
+			),
+			// define a transition from one state to another, and an associated animation
+			transition('* => *', animate('5s 0s ease-in'))
+		])
+	]
+})
+export class AppComponent {
+	title = 'Stock Dashboard';
+	moving: string;
+	moveIt() {
+		console.log('on the move');
+		// change the state of moving to roaming, activating the trigger and animation
+		this.moving = 'roaming';
+	}
+}
